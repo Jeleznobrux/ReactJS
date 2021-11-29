51e197a1eea1997e61d92e191527285289e1cf4a
Jeleznobrux <86488668+Jeleznobrux@users.noreply.github.com>
2021-11-30 00:44:31 +0700

    Delete src directory

--- src/components/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class Message extends React.Component {
-    render() {
-        return <div style={{
-            background: "violet"
-        }}> {this.props.message} </div>
-    }
-}
